355e734e9f3cec6c6f55afbfa8257b2ab24ece52
UHyeonj <dngusdlqwkd@gmail.com>
2025-07-22 01:23:44 +0900

    테스트

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,7 @@ import LogInBtn from "./LoginBtn";
 import MainPageLogo from "./MainPageLogo";
 
 const NavBar = () => {
+  const title = "서울 여유 여행";
   return (
     <nav className="flex justify-between bg-white shadow-sm border-b border-gray-200 p-4">
       <MainPageLogo />
