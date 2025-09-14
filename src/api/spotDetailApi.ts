65db5ce644ad85983829f64e4a4dd8ddeeeacf63
UHyeonj <dngusdlqwkd@gmail.com>
2025-09-14 22:22:45 +0900

    fix: 통신 복귀

--- src/api/spotDetailApi.ts
+++ src/api/spotDetailApi.ts
@@ -5,31 +5,33 @@ export const getTourSpotDetail = async ({
 }: {
   tourspotId: number;
 }) => {
-  const response = await baseAxiosInstance.get(`/api/tourspot`, {
-    params: { tourspotId },
-  });
+  const response = await baseAxiosInstance.get(
+    `/api/tourspot/${tourspotId}`,
+    {},
+  );
   return response.data;
 };
 
 export const getIsHeart = async (tourspotId: number) => {
-  const response = await baseAxiosInstance.get(`/mypage/preferences/check`, {
-    params: { tourspotId },
-  });
+  const response = await baseAxiosInstance.get(
+    `/mypage/preferences/check/${tourspotId}`,
+    {},
+  );
   return response.data;
 };
 
 export const addHeart = async (tourspotId: number) => {
   const response = await baseAxiosInstance.post(
-    `/mypage/preferences`,
+    `/mypage/preferences/${tourspotId}`,
     {},
-    { params: { tourspotId } },
   );
   return response.data;
 };
 
 export const deleteHeart = async (tourspotId: number) => {
-  const response = await baseAxiosInstance.delete(`/mypage/preferences`, {
-    params: { tourspotId },
-  });
+  const response = await baseAxiosInstance.delete(
+    `/mypage/preferences/${tourspotId}`,
+    {},
+  );
   return response.data;
 };
