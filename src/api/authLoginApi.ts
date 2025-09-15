d0312aa52a5ae1f60b3b3fa35672ad9d2c207c9d
UHyeonj <dngusdlqwkd@gmail.com>
2025-09-16 01:21:02 +0900

    fix: 로그인확인 api 수정

--- src/api/authLoginApi.ts
+++ src/api/authLoginApi.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL;
-
 export const getLoginInfo = async () => {
-  const response = await axios.get(`${baseURL}/auth/test`, {
-    withCredentials: true, // 서버에서 내려준 httpOnly 쿠키 포함
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  });
-  return response.data; // 필요한 데이터만 반환
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_API_BASE_URL}/auth/test`,
+      { withCredentials: true },
+    );
+    return response.data;
+  } catch (error: any) {
+    if (error.response?.status === 401) return undefined;
+    throw error;
+  }
 };
